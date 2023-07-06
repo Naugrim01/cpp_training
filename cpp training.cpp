5cff6ca194b437dac1c5b6238be4231c0860d404
Lukasz Pietrzykowski <lukasz.pietrzykowski1988@gmail.com>
2023-07-06 23:04:12 +0200

    added prime numbers generation function

--- cpp training.cpp
+++ cpp training.cpp
@@ -3,10 +3,12 @@
 
 
 void is_prime_number();
+void prime_number_generator();
+
 int main()
 {
-    std::cout << "Hello World!\n";
-    is_prime_number();
+    //is_prime_number();
+    prime_number_generator();
 
     system("pause");
     return 0;
@@ -36,4 +38,30 @@ void is_prime_number()
         if (check)
             std::cout<<"this number is prime number\n";
 
+}
+
+void prime_number_generator()
+{
+    bool check = true;
+    int a = 2, b = 0;
+    std::cout << "Please enter the number of prime numbers to generate:\n";
+    std::cin >> b;
+    std::cin.ignore();
+    for (unsigned int i = 0; i < b; i++)
+    {
+        check = true;
+        for (unsigned int j = 2; j <= sqrt(a); j++)
+        {
+            if (!(a % j))
+            {
+                check = false;
+                break;
+            }
+        }
+        if (check)
+            std::cout << i + 1 <<". "<< a << "\n";
+        else
+            i--;
+        a++;
+    }
 }
